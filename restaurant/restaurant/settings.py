41496f978b443f7c14caabfd569bfb88f7ee8de7
pravintp <pravin@testpress.in>
2021-12-30 17:14:57 +0530

    Update app name

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "shop.apps.ShopConfig",
+    "my_restaurant.apps.MyRestaurantConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
