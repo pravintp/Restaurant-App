684aecdd512792fd046d3963a0cb88e8c22b9451
pravintp <pravin@testpress.in>
2022-01-03 18:50:03 +0530

    Add list view

--- restaurant/my_restaurant/views.py
+++ restaurant/my_restaurant/views.py
@@ -1,3 +1,18 @@
+from django.db.models import Count, Avg
 from django.shortcuts import render
 
+from .models import Restaurant, Photo
+
 # Create your views here.
+
+
+def restaurant_list_view(request):
+    return render(
+        request,
+        "my_restaurant/list.html",
+        {
+            "restaurants": Restaurant.objects.annotate(
+                nreviews=Count("reviews"), avg=Avg("reviews__rating")
+            )
+        },
+    )
